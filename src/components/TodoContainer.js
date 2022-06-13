200d0dcd43310c0bc708c8eff5360beee3f45a69
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-13 20:18:04 +0100

    Add codes in todo container

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 class TodoContainer extends React.Component {
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+       };
     render() {
         return (
             <>
-                <h1>Create React App </h1>
-                <p>I'm in React component</p>
+            {this.state.todos.map(todo => (
+                <li>{todo.title}</li>
+                ))}
             </>
         );
     }
