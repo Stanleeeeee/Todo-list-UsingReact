0dcca08cdf485aae23987e499e672c1c8a0f29c2
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-13 15:19:45 +0100

    Add Todo index.js imports and adding React StrictMoode

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,10 +3,10 @@ import React from 'react';
 class TodoContainer extends React.Component {
     render() {
         return (
-            <div>
+            <>
                 <h1>Create React App </h1>
                 <p>I'm in React component</p>
-            </div>
+            </>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoCointainer from './components/TodoContainer';
+import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<TodoContainer />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />,
+  </React.StrictMode>,
+  document.getElementById('root')
+)
