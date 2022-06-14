5ec09df78c0bd783ee388f6e222d363062c61c2f
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-14 07:06:19 +0100

    Add props functionality to app

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+    render() { 
+        return ( 
+            <>
+            <h1>I am learning react</h1>
+            </>
+         );
+    }
+}
+ 
+export default TodoItem;
