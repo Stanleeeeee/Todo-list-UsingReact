ba17a2e67a3acadc17bf8b3f0eae7c5105194e20
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-18 18:30:00 +0100

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoContainer from './components/TodoContainer';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <TodoContainer />,
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
