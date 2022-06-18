20576b3a76eb9f3c73b13864d757852961df965f
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-18 18:31:20 +0100

    Add functionalities of Yodo app

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,16 +1,37 @@
-import React from "react"
-import TodoItem from "./TodoItem";
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
+class TodosList extends React.PureComponent {
   render() {
+    const {
+      handleChangeProps, deleteTodoProps, setUpdate, todos,
+    } = this.props;
     return (
       <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} />
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={handleChangeProps}
+            deleteTodoProps={deleteTodoProps}
+            setUpdate={setUpdate}
+          />
         ))}
+
       </ul>
-    )
+    );
   }
 }
 
-export default TodosList
+TodosList.propTypes = {
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    completed: PropTypes.bool,
+  })).isRequired,
+};
+export default TodosList;
